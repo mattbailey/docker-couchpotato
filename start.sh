e040f30f0a4d2410b1a3df88ce9292a6d024f32c
Matt Bailey <m@mdb.io>
2015-02-09 11:48:59 -0800

    stfu, python

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ cd /CouchPotatoServer
 touch /config/CouchPotato.cfg
 mkdir -p /config/data 2> /dev/null
 
-/usr/bin/python /CouchPotatoServer/CouchPotato.py --data_dir /config/data --config_file /config/CouchPotato.cfg --console_log
+/usr/bin/python /CouchPotatoServer/CouchPotato.py --data_dir /config/data --config_file /config/CouchPotato.cfg --console_log --quiet
